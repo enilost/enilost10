0c3a8a10304d621f9397adaefe9b5570b4a74c7a
Andrey <enilost@gmail.com>
2021-12-15 12:15:08 +0300

    initid

---
+++ precache-manifest.6885d3796a224c6b039dbe5259e353cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e395efc1d8931c5d0b2d",
+    "url": "/css/app.e2efaea1.css"
+  },
+  {
+    "revision": "baeb9ee4702c95549f043348fcedbb83",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "d8bc987dee27bb2bd0117d28899ed261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e395efc1d8931c5d0b2d",
+    "url": "/js/app.bd307d00.js"
+  },
+  {
+    "revision": "19844b9fd8758085555a",
+    "url": "/js/chunk-vendors.ba5f1dfe.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
